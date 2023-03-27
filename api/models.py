abe6481f41ec752a4338e943a6e30de0948ba555
Jack Glenn <jackg627@gmail.com>
2023-03-27 13:55:46 -0600

    Added more pydantic model classes to api/models.py

--- api/models.py
+++ api/models.py
@@ -1,6 +1,6 @@
 from bson.objectid import ObjectId
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
 class PydanticObjectId(ObjectId):
     @classmethod
@@ -23,11 +23,43 @@ class ResortIn(BaseModel):
     zipcode: int
     name: str
     photo_url: str
-    Average_rating: float
-    Elevation: int
-    Price: str
+    average_rating: float
+    elevation: int
+    price: str
 
 class ResortOut(ResortIn):
     id: str
 
+class AccountIn(BaseModel):
+    first_name: str
+    last_name: str
+    user_name: str
+    password: str
+    email: str
+    address: str
+    city: str
+    state: str
+    zipcode: int
+    ski: bool
+    snowboard: bool
+    picture_url: Optional[str] = None
 
+class AccountOut(AccountIn):
+   id: str
+
+class CommentIn(BaseModel):
+	rating: int
+	comment: str
+	user_id: str #reference to account id
+	resort_id: str #reference to resort id
+
+class CommentOut(CommentIn):
+    id: str
+
+class FavoriteIn(BaseModel):
+    favorite: bool
+    user_id: str #reference to account id
+    resort_id: str #reference to resort id
+
+class FavoriteOut(FavoriteIn):
+    id: str
