cab202723d7de1117fff56b5fb7f18693d6e3afc
Anton <pl219051@gmail.com>
2023-04-06 15:21:22 -0400

    committing changes for eric's edit

--- ghi/src/components/login/LoginForm.js
+++ ghi/src/components/login/LoginForm.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { handlePasswordChange, handleUsernameChange, reset,} from "./loginSlice";
+import {
+  handlePasswordChange,
+  handleUsernameChange,
+  error,
+  reset,
+} from "./loginSlice";
 import { useLoginMutation } from "./auth";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import ErrorMessage from "./ErrorMessage";
 
 const LoginForm = () => {
   const dispatch = useDispatch();
   const [login] = useLoginMutation();
-  const { fields } = useSelector((state) => state.login);
-  const navigate = useNavigate()
+  const { errorMessage, fields } = useSelector((state) => state.login);
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,14 +23,19 @@ const LoginForm = () => {
     login(fields);
     dispatch(reset());
 
-    const result = await login(fields).unwrap()
-    console.log(result)
+    const result = await login(fields).unwrap();
+    console.log(result);
     if (result.access_token) {
-      navigate("/home")
-    } else {
-      alert("Incorrect username or password, please try again you fat-fingered fool")
+      navigate("/home");
     }
-
+    if (!result.access_token) {
+      dispatch(
+        error(
+          "Incorrect username or password, please try again you fat-fingered fool"
+        )
+      );
+      return;
+    }
   };
 
   return (
@@ -33,6 +44,7 @@ const LoginForm = () => {
         <div className="shadow p-4 mt-4">
           <h1>Login</h1>
           <form onSubmit={handleSubmit} id="login-form">
+            {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
             <div className="form-floating mb-3">
               <input
                 value={fields.username}
@@ -59,6 +71,6 @@ const LoginForm = () => {
       </div>
     </div>
   );
-}
+};
 
 export default LoginForm;
