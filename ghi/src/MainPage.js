4ed1c1f4680f053c970150041f0b8125e5962054
Eric Mills <ericjosephmills@gmail.com>
2023-04-06 12:51:05 -0500

    tweaked main page

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -1,25 +1,36 @@
 import Footer from "./Footer.js";
-import { useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 function MainPage() {
   const navigate = useNavigate();
 
   const handleJoinClick = () => {
-    navigate("/signup")
-  }
+    navigate("/signup");
+  };
 
   return (
     <>
-      
       <br></br>
       <br></br>
       <h1 className="snow">SkiColorado</h1>
       <h2>The ultimate community of Colorado ski and snowboard enthusiasts!</h2>
-      <h3>View current conditions, explore new mountains, and so much more!</h3>
+      {/* <h3>View current conditions, explore new mountains, and so much more!</h3> */}
+      <div>
+        <h3>
+          Already have an account?&nbsp;&nbsp;
+          <a href="#" class="link-warning">
+            Click here!
+          </a>
+        </h3>
+      </div>
       <br />
       <h3>
-        <button className="butt btn-primary" onClick={handleJoinClick}>Join Now!</button>
+        <button className="butt btn-primary" onClick={handleJoinClick}>
+          Join Now!
+        </button>
       </h3>
+      <br />
+
       <Footer />
     </>
   );
