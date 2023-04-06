92b7c1ac30e3c1cd16228b90603960e24cdc6c8c
Jack Glenn <jackg627@gmail.com>
2023-04-06 13:49:26 -0400

    modifield logged-in navbar

--- ghi/src/components/header/NavLoggedIn.js
+++ ghi/src/components/header/NavLoggedIn.js
@@ -20,6 +20,38 @@ function NavLoggedIn() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+        <div className="snow">
+          <NavLink className="navbar-brand" to="/profile">
+            *ProfileLinkImage*
+          </NavLink>
+        </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+                <div className="snow">
+          <NavLink className="navbar-brand" to="/">
+            SignoutButton
+          </NavLink>
+        </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <ul></ul>
