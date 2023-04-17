bbf63c56511b3fb9498ef1d386ea023cd172a703
Jack Glenn <jackg627@gmail.com>
2023-04-17 19:20:26 -0400

    In ResortDetail.js, added functionality to filter comments by individual resort

--- ghi/src/components/resort/ResortDetail.js
+++ ghi/src/components/resort/ResortDetail.js
@@ -2,7 +2,8 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useGetResortQuery, useGetCommentsQuery } from "../login/auth.js";
 import NavLoggedIn from "../header/NavLoggedIn.js";
-import CommentList from "../comments/CommentList.js";
+import ResortFilteredCommentList from "../comments/ResortFilteredCommentList.js";
+import IndividualComment from "../comments/IndividualComment.js";
 
 
 
@@ -11,7 +12,7 @@ import CommentList from "../comments/CommentList.js";
 export default function ResortDetail() {
   const { thisResort } = useParams();
   const { data, error, isLoading } = useGetResortQuery(thisResort);
-  console.log(data);
+  
 
   if (!data) {
     return <div>Loading...</div>;
@@ -49,7 +50,7 @@ export default function ResortDetail() {
         </div>
         <div className="col-auto">
           <div>
-            <CommentList/>
+            <ResortFilteredCommentList comments={data.comments} thisResort={thisResort} />
           </div>
         </div>
       </div>
