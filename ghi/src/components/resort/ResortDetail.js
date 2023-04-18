3dbe9068cd48839c8f14802133a28de694241e89
Alex <adrosos323@gmail.com>
2023-04-18 12:50:22 -0600

    wrote comma function in resortdetail and journal

--- ghi/src/components/resort/ResortDetail.js
+++ ghi/src/components/resort/ResortDetail.js
@@ -6,14 +6,9 @@ import ResortFilteredCommentList from "../comments/ResortFilteredCommentList.js"
 import IndividualComment from "../comments/IndividualComment.js";
 import AverageRatingByResort from "../comments/AverageRatingFunction.js";
 
-
-
-
-
 export default function ResortDetail() {
   const { thisResort } = useParams();
   const { data, error, isLoading } = useGetResortQuery(thisResort);
-
 
   if (!data) {
     return <div>Loading...</div>;
@@ -22,6 +17,16 @@ export default function ResortDetail() {
   function dollarPrice(resortprice) {
     return "$".repeat(resortprice);
   }
+
+  function addComma(num) {
+    const str = num.toString();
+    if (str.length == 5 || str.length == 4) {
+      return str.slice(0, -3) + "," + str.slice(-3);
+    } else {
+      return num;
+    }
+  }
+
   return (
     <>
       <NavLoggedIn />
@@ -39,8 +44,8 @@ export default function ResortDetail() {
               <br></br> {data?.city}, {data?.state} {data?.zipcode}
             </p>
             <p className="text-center">
-              Top Elevation: {data?.elevation} ft<br></br>Vertical Drop:{" "}
-              {data?.vertical_drop} ft<br></br>Number of Trails:{" "}
+              Top Elevation: {addComma(data?.elevation)} ft<br></br>Vertical
+              Drop: {addComma(data?.vertical_drop)} ft<br></br>Number of Trails:{" "}
               {data?.num_trails}
             </p>
             <p className="text-center">
@@ -51,10 +56,13 @@ export default function ResortDetail() {
         </div>
         <div className="col-auto">
           <div>
-            <ResortFilteredCommentList comments={data.comments} thisResort={thisResort} />
+            <ResortFilteredCommentList
+              comments={data.comments}
+              thisResort={thisResort}
+            />
           </div>
           <div>
-            <AverageRatingByResort/>
+            <AverageRatingByResort />
           </div>
         </div>
       </div>
--- journals/alex_drosos.md
+++ journals/alex_drosos.md
@@ -4,5 +4,6 @@
 3/30/23 - Began work on design of SkiColorado main page. Worked through some issues with package installations.
 4/3/23 - Continued work on main page with goal to finish. Collaborated with Eric to tweak existing elements until we were happy with product.
 4/4/23 - Implemented few additional features to main page with Eric, including raining snow on page. Compiled all resort data into formatted strings for future ease of populating database.
-
+4/7/23 - wrote function and added fields in resortdetail.js
 4/17/23 - Played around some more with twitter's timeline embedding generator code and could not get it to display. Found and installed the react-twitter-widgets npm package. Tweaked setting and framed the widget in matching styling so that it would fit in with other elements on the page.
+4/18/23 - Met with Eric and came up with final solution for positioning/spacing on the resort list page. Previously, I had worked on implementing the "toLocaleString()" method to add correct commas to resort statistic numbers in the thousands, however, on refresh it would break the page. Never found a solution so today I wrote a function from scratch that accomplishes what I was looking for without breaking the webpage.
