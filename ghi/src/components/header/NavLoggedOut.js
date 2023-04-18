a414eadbf272430aefa0b98c26acb5ef38da2548
Eric Mills <ericjosephmills@gmail.com>
2023-04-18 15:57:28 -0500

    updated navbar

--- ghi/src/components/header/NavLoggedOut.js
+++ ghi/src/components/header/NavLoggedOut.js
@@ -9,22 +9,6 @@ function NavLoggedOut() {
             SC
           </NavLink>
         </div>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <ul></ul>
-          </ul>
-        </div>
       </div>
     </nav>
   );
