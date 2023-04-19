5feded475abcc2dee88aa54107365f3e5bc53bc3
Richard <chong.rc@gmail.com>
2023-04-19 17:19:07 -0400

    Got all the currently available, necessary data to show on the frontend

--- ghi/src/components/profile/ProfileInfo.js
+++ ghi/src/components/profile/ProfileInfo.js
@@ -10,17 +10,35 @@ export default function ProfileInfo() {
   const { data } = useGetProfileQuery(accountId);
 
   const { data: account } = useGetAccountQuery();
+  let ski = ""
+  let snowboard = ""
+  if (data?.ski == true){
+    ski = "Is a Skier"
+  } else {
+    ski = "Not a Skier"
+  }
+  if (data?.snowboard == true) {
+    snowboard = "Is a Snowboarder"
+  } else {
+    snowboard = "Not a Snowboarder"
+  }
 
   if (data?.id == account?.id) {
     return (
       <div>
-        <h3>Info</h3>
+        <h2>Info</h2>
         {account && data && (
           <div>
             <h3>
               {data.address_1}
               <br />
               {data.city} {data.state}, {data.zipcode}
+            </h3>
+            <br />
+            <h3>
+              {ski}
+              <br />
+              {snowboard}
             </h3>
           </div>
         )}
@@ -31,11 +49,15 @@ export default function ProfileInfo() {
   else {
     return (
       <div>
-        <h3>Info</h3>
+        <h2>Info</h2>
         {account && data && (
           <div>
+            <h3>{data.city}, {data.state}</h3>
+            <br/>
             <h3>
-              {data.city}, {data.state}
+                {ski}
+                <br/>
+                {snowboard}
             </h3>
           </div>
         )}
