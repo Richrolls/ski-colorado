e6c822fa6ced43efdcf12b2551dfc51a70f47648
Alex <adrosos323@gmail.com>
2023-04-27 15:01:08 -0600

    fixed console error on logout

--- ghi/src/components/homepage/Home.js
+++ ghi/src/components/homepage/Home.js
@@ -179,35 +179,35 @@ function HomePage() {
           <br />
           <br />
         </h5>
-      <div class="container">
-        <div className="row">
-          <div className="offset-3 col-6">
-            <div
-              className="shadow p-4 mt-4 bg-primary bg-gradient text-center"
-              style={{ borderRadius: 8 }}
-            >
-              <h1 className="snow">Unauthorized</h1>
-              <h2>Please log in or sign up.</h2>
-              <br />
-              <div>
-                <img className="resort-photo rounded" src={Closed}></img>
+        <div className="container">
+          <div className="row">
+            <div className="offset-3 col-6">
+              <div
+                className="shadow p-4 mt-4 bg-primary bg-gradient text-center"
+                style={{ borderRadius: 8 }}
+              >
+                <h1 className="snow">Unauthorized</h1>
+                <h2>Please log in or sign up.</h2>
+                <br />
+                <div>
+                  <img className="resort-photo rounded" src={Closed}></img>
+                </div>
+                <br />
+                <h3>
+                  <Link to="/login" className="link-warning">
+                    Click here to log in.
+                  </Link>
+                </h3>
+                <br />
+                <h3>
+                  <Link to="/signup" className="link-warning">
+                    Click here to sign up.
+                  </Link>
+                </h3>
               </div>
-              <br />
-              <h3>
-                <Link to="/login" className="link-warning">
-                  Click here to log in.
-                </Link>
-              </h3>
-              <br />
-              <h3>
-                <Link to="/signup" className="link-warning">
-                  Click here to sign up.
-                </Link>
-              </h3>
             </div>
           </div>
         </div>
-      </div>
       </div>
     );
   }
