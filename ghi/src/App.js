919759ff88f662fd6eb5e2e70cb1a6bc67082d01
Jack Glenn <jackg627@gmail.com>
2023-04-28 16:34:36 -0400

    Code cleanup

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,7 +1,4 @@
-import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route, Form } from "react-router-dom";
-import Construct from "./Construct.js";
-import ErrorNotification from "./components/errorhandling/ErrorNotification";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./components/mainpage/MainPage.js";
 import Signup from "./components/signup/Signup.jsx";
 import ResortDetail from "./components/resort/ResortDetail.js";
@@ -13,24 +10,6 @@ import NotFound from "./components/errorhandling/404Page.js";
 import "./App.css";
 
 function App() {
-  // const [launch_info, setLaunchInfo] = useState([]);
-  // const [error, setError] = useState(null);
-
-  // useEffect(() => {
-  //   async function getData() {
-  //     let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-  //     let response = await fetch(url);
-  //     let data = await response.json();
-
-  //     if (response.ok) {
-  //       setLaunchInfo(data.launch_details);
-  //     } else {
-  //       setError(data.message);
-  //     }
-  //   }
-  //   getData();
-  // }, []);
-
   return (
     <>
       <BrowserRouter>
